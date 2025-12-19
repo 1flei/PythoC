d687e44b29d296b43446f47f3155d6f9102778e3
github-action-benchmark <github@users.noreply.github.com>
2025-12-19 11:55:40 +0000

    add PythoC Performance (customSmallerIsBetter) benchmark result for 73bad2fb08d53e0b254cd85b11e4ba41daa90bba

---
+++ dev/bench/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1766145340276,
+  "repoUrl": "https://github.com/1flei/PythoC",
+  "entries": {
+    "PythoC Performance": [
+      {
+        "commit": {
+          "author": {
+            "email": "yfleiii@gmail.com",
+            "name": "1flei",
+            "username": "1flei"
+          },
+          "committer": {
+            "email": "yfleiii@gmail.com",
+            "name": "1flei",
+            "username": "1flei"
+          },
+          "distinct": true,
+          "id": "73bad2fb08d53e0b254cd85b11e4ba41daa90bba",
+          "message": "Address CI for arm",
+          "timestamp": "2025-12-19T19:47:13+08:00",
+          "tree_id": "075e6e5dbb18fc05322b029f99c912abbb7db67b",
+          "url": "https://github.com/1flei/PythoC/commit/73bad2fb08d53e0b254cd85b11e4ba41daa90bba"
+        },
+        "date": 1766145339985,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "binary_tree_c",
+            "value": 6.7439,
+            "unit": "seconds"
+          },
+          {
+            "name": "binary_tree_pc",
+            "value": 5.42,
+            "unit": "seconds"
+          },
+          {
+            "name": "nsieve_c",
+            "value": 4.429,
+            "unit": "seconds"
+          },
+          {
+            "name": "nsieve_pc",
+            "value": 4.5635,
+            "unit": "seconds"
+          }
+        ]
+      }
+    ]
+  }
+}
