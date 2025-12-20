f64fad8be9997f2e2907e5eb6e955951d68d06a6
github-action-benchmark <github@users.noreply.github.com>
2025-12-20 03:31:06 +0000

    add PythoC Performance (customSmallerIsBetter) benchmark result for 2bc3077801061dd45001d5ed95f3561681056300

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1766145340276,
+  "lastUpdate": 1766201466079,
   "repoUrl": "https://github.com/1flei/PythoC",
   "entries": {
     "PythoC Performance": [
@@ -43,6 +43,50 @@ window.BENCHMARK_DATA = {
           {
             "name": "nsieve_pc",
             "value": 4.5635,
+            "unit": "seconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "yfleiii@gmail.com",
+            "name": "yifan",
+            "username": "1flei"
+          },
+          "committer": {
+            "email": "yfleiii@gmail.com",
+            "name": "yifan",
+            "username": "1flei"
+          },
+          "distinct": true,
+          "id": "2bc3077801061dd45001d5ed95f3561681056300",
+          "message": "Address some windows spec problem",
+          "timestamp": "2025-12-20T11:28:35+08:00",
+          "tree_id": "ddbdfa10677ccb3365a3ea4d4a6e24dbca2fe06e",
+          "url": "https://github.com/1flei/PythoC/commit/2bc3077801061dd45001d5ed95f3561681056300"
+        },
+        "date": 1766201465792,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "binary_tree_c",
+            "value": 6.7172,
+            "unit": "seconds"
+          },
+          {
+            "name": "binary_tree_pc",
+            "value": 5.5072,
+            "unit": "seconds"
+          },
+          {
+            "name": "nsieve_c",
+            "value": 4.5634,
+            "unit": "seconds"
+          },
+          {
+            "name": "nsieve_pc",
+            "value": 4.7797,
             "unit": "seconds"
           }
         ]
