d575470cd64f56dfcb60245b2870e8421e4e4c03
1flei <yfleiii@gmail.com>
2026-02-12 11:41:16 +0800

    Add windows CI

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     python_requires='>=3.8',
     install_requires=[
         'llvmlite>=0.40.0,<0.45.0',
+        'ziglang>=0.13.0; sys_platform == "win32"',
     ],
     extras_require={
         'dev': [
